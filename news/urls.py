14b477ba174f6a53ff110e7454250f26871a1496
duchung <tdhabcdef1710@gmail.com>
2025-11-03 16:22:15 +0700

    chinh lai link den trang bai viet

--- news/urls.py
+++ news/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.news_list, name='news'),
+    path('', views.news_list, name='news_homepage'),
     path('<slug:slug>/', views.news_detail, name='news_detail'),
 ] 
